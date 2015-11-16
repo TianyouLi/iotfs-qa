78447b5b3bfe8b899f0b8d943eb254272ffd3792
Tianyou Li <tianyou.li@gmail.com>
2015-11-16 05:06:16 +0000

    generate coverage report automatically

--- build/build.sh
+++ build/build.sh
@@ -7,3 +7,16 @@ cmake ../unit
 make
 cd ${CUR_DIR}
 
+# test
+cd ${BLD_DIR}
+make test
+cd ${CUR_DIR}
+
+
+# generate coverage report for unit test
+cd ${BLD_DIR}
+lcov --capture --directory .. --output-file unit_coverage.info
+lcov --remove unit_coverage.info 'boost/*' 'c++/*' 'include/gtest/*' '*/iotfs-qa/*' '*/iotfs/deps/*' 'src' -o unit_coverage_filtered.info
+genhtml unit_coverage_filtered.info --output-directory ../reports/coverage/unit
+cd ${CUR_DIR}
+
